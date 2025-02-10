71dea1feae9fb5b1407a78cc5acf2ef0c5c1ddd6
Hamza Khan <Hamza Khan@HAMZA>
2025-02-10 20:22:40 +0500

    Add project files.

---
+++ Models/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Models;
+
+namespace Models.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var studentD = new List<Student>()
+            {
+                new Student {Id = 1, Name = "Hamza", FatherName = "Jalil"},
+                new Student {Id = 2, Name = "Faryal", FatherName = "Waseem"},
+                new Student {Id = 3, Name = "Bilal", FatherName = "Zafar"}
+            };
+
+            ViewBag.StudentData = studentD;
+            return View();
+        }
+    }
+}
---
+++ Models/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Models.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+    }
+}
